6a61bb7364853f80a0e1b5660c4cc84a6c61e61c
Prashant <ppresh.ant@gmail.com>
2017-02-07 01:43:23 -0600

    bare code runs but not learning anything - 1000 iterations

---
+++ hw 4 files/run_bare.m
@@ -0,0 +1,8 @@
+%% make XOR patterns
+X = [0 0 1;1 0 1;0 1 1;1 1 1]';
+Y = [0 1 1 0];
+%% Run backprog algo
+[w1,w2] = bare_backprop(X,Y);
+%% Verify result
+outpt = tanh(w2*[tanh(w1*X);1 1 1 1])
+desired_output = Y
