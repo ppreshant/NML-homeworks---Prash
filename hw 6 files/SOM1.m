029e96434c7704e4e7c7eef11274d3de6b9c1e25
Prashant <ppresh.ant@gmail.com>
2017-03-06 02:08:57 -0600

    Working SOM - tested on optional problem 1

--- hw 6 files/SOM1.m
+++ hw 6 files/SOM1.m
@@ -43,6 +43,9 @@ function finalLattice = selfOrganize(latticeCell,dataInput,numIters,initRadius,a
 Z = cellfun(@(x)reshape(x,1,1,[]),latticeCell,'un',0);
 lattice = cell2mat(Z); % this is a multi-dimensional Matrix, with third dimension holding different input dimensions
 
+r = (1:size(lattice,1))';c = 1:size(lattice,2); 
+latticeIndices(:,:,1) = r(:,ones(1,size(lattice,2))); latticeIndices(:,:,2) = c(ones(1,size(lattice,1)),:);  % latticeIndices : holds the i,j indices of the 2d lattice space
+
 figure;
 subplot(2,2,1);
 plot(dataInput(1,:),dataInput(2,:),'g.'); hold on; plot(lattice(:,:,1),lattice(:,:,2),'ko','MarkerFaceColor','k','MarkerSize',4);
@@ -55,27 +58,20 @@ for i = 1:numIters
 %     radius = initRadius; % can do decay here
     radius = initRadius * ((i <= numIters/5) + .8 * (i > numIters/5 & i <= numIters/2) + .5 * (i > numIters/2 & i <= numIters*.8)+ .2 * (i > numIters*.8));
     alpha = alphaI * ((i <= numIters/10) + .5 * (i > numIters/10 & i <= numIters/2.5) + .125 * (i > numIters/2.5 & i <= numIters*.8)+ .025 * (i > numIters*.8));
-
-    differenceMatrix = zeros(size(lattice)); % a 3D matrix
-    distToXMatrix =  zeros(size(latticeCell)); % a 2D matrix of eucledian distances
       
     % pick an x (data point) randomly
     x = dataInput(:,randi(size(dataInput,2)));
    
     % find euclidian distances and difference between chosen x and all W's
-    for k = 1:size(lattice,2)
-        for j = 1:size(lattice,1)
-            differenceMatrix(j,k,:) = x - reshape(lattice(j,k,:),size(x));
-            distToXMatrix(j,k) = norm(reshape(differenceMatrix(j,k,:),size(x)));
-        end
-    end
-    
+    differenceMatrix = reshape(x,1,1,[]) - lattice; % a 3D matrix of difference between every weight and x
+    distToXMatrix = sqrt(sum((differenceMatrix).^2,3)); % finding norm or eucledian distance
+  
     % find the winner = c = [win_row win_col]
     [~, winner] = min(distToXMatrix(:)); [win_row, win_col] = ind2sub(size(distToXMatrix), winner); 
     c = [win_row win_col];
     
     % make a neighbourhood function in a matrix
-    neighbourhoodFn = makeNeighbourhoodFn(lattice,c,radius);
+    neighbourhoodFn = makeNeighbourhoodFn(latticeIndices,c,radius);
     
     % update the weights - Learning rule
     lattice = lattice + alpha * neighbourhoodFn .* differenceMatrix;
@@ -98,37 +94,29 @@ end
 function densityLattice = calcDensityLattice(lattice,dataInput,sizeOflatticeCell)
 
 densityLattice = zeros(sizeOflatticeCell);
-distToXMatrix = zeros(sizeOflatticeCell); % a 2D matrix of eucledian distances
+
 for i = 1:size(dataInput,2)
     x = dataInput(:,i);
     
     % find euclidian distances and difference between chosen x and all W's
-    for k = 1:size(lattice,2)
-        for j = 1:size(lattice,1)
-            distToXMatrix(j,k) = norm(x - reshape(lattice(j,k,:),size(x)));
-        end
-    end
+    differenceMatrix = reshape(x,1,1,[]) - lattice; % a 3D matrix
+    distToXMatrix = sqrt(sum((differenceMatrix).^2,3)); % a 2D matrix for euclidian distances to x
     
     % find the winner = c = [win_row win_col]
     [~, winner] = min(distToXMatrix(:)); [win_row, win_col] = ind2sub(size(distToXMatrix), winner); 
     c = [win_row win_col];
+    % update the density lattice
     densityLattice(c(1),c(2)) = densityLattice(c(1),c(2)) + 1;
 end
     
 end
 
 
-function neighbourhoodFn = makeNeighbourhoodFn(lattice,c,radius)
+function neighbourhoodFn = makeNeighbourhoodFn(latticeIndices,c,radius)
 
-neighbourhoodFn = zeros(size(lattice));
-for j = 1:size(lattice,2)
-    for i = 1:size(lattice,1)
-        w = [i j];
-        distNeighbour = sum(abs(w - c)); % Manhattan distance metric for the neighbourhood function
-        %     distNeighbour = norm(w,c); % eucleidian distance metric for the neighbourhood function
-        neighbourhoodFn(i,j,:) = exp(-((distNeighbour)/(radius))^2);
-    end
-end
+distNeighbour = sum(abs(latticeIndices - reshape(c,1,1,[])),3); % Manhattan distance metric for the neighbourhood function
+% EqDistNeighbour = sqrt(sum((latticeIndices - reshape(c,1,1,[])).^2,3)); % eucleidian distance metric for the neighbourhood function
+neighbourhoodFn = exp(-((distNeighbour)./(radius)).^2);
 
 end
 
@@ -148,3 +136,23 @@ end
 % distNeighbour = @(w,c) sum(abs(w - c)); % Manhattan distance metric for the neighbourhood function
 % %     distNeighbour = @(w,c) norm(w,c); % eucleidian distance metric for the neighbourhood function
 % neighbourhoodFn = @(w,c) exp(-((distNeighbour(w,c))/(2*radius))^2);
+% 
+% Initializations
+%     differenceMatrix = zeros(size(lattice)); % a 3D matrix
+%     distToXMatrix =  zeros(size(latticeCell)); % a 2D matrix of eucledian distances
+%     
+% long for loop for calculation of eucliean distances         
+%     for k = 1:size(lattice,2)
+%         for j = 1:size(lattice,1)
+% %             differenceMatrix(j,k,:) = x - reshape(lattice(j,k,:),size(x));
+%             distToXMatrix(j,k) = norm(reshape(differenceMatrix(j,k,:),size(x)));
+%         end
+%     end
+% 
+% long for loop for finding distance and making neighbourhood functions
+% for j = 1:size(lattice,2)
+%     for i = 1:size(lattice,1)
+%         w = [i j];
+% %         distNeighbour = sum(abs(w - c)); % Manhattan distance metric for the neighbourhood function
+%         %     distNeighbour = norm(w,c); % eucleidian distance metric for the neighbourhood function
+%         neighbourhoodFn(i,j,:) = exp(-((distNeighbour)/(radius))^2);
